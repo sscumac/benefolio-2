16e3e71776146ed941d2aa4def5a10a71e61a4d7
sscumac <sebastian.schumacher@hotmail.de>
2025-04-17 18:03:53 +0200

    add image

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,8 @@
 <template>
   <div class="flex min-h-screen flex-col sm:overflow-hidden">
     <OrganismsHeader class="fixed z-20" />
-    <main
-      id="main"
-      style="
-        background-image: url(&quot;/linien.png&quot;);
-        background-size: cover;
-        background-repeat: repeat;
-        background-size: contain;
-      "
-    >
+    <main id="main">
+      <img src="~/assets/linien.png" alt="background" class="fixed object-cover min-h-screen opacity-80" />
       <slot />
     </main>
   </div>
