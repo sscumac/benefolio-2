51ad398049e56c9a2609d0d111d18163c0dbcfa6
sscumac <sebastian.schumacher@hotmail.de>
2025-04-17 15:24:59 +0200

    background image

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="flex min-h-screen flex-col sm:overflow-hidden">
     <OrganismsHeader class="fixed z-20" />
-    <main id="main">
-      <img src="~/assets/Vectordots.png" alt="background" class="fixed object-cover min-h-screen opacity-80" />
+    <main
+      id="main"
+      style="
+        background-image: url(&quot;/linien_hand.png&quot;);
+        background-size: cover;
+        background-repeat: repeat;
+        background-size: contain;
+      "
+    >
       <slot />
     </main>
   </div>
